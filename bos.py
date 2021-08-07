9f8924ba39f4f6361abbffa870363857a38ca52f
athensmusa <athensmusa@gmail.com>
2021-08-07 13:29:17 +0300

    python

--- bos.py
+++ bos.py
@@ -1,10 +1,2 @@
-even=[]
-odd=[]
-for i in range(0,11):
-    if i%2==0:
-        even.append(i)
-     
-    else:
-        odd.append(i)
-print(even)
-print(odd)       
+for x inset('pqr'):
+    print(x*2)
